251e8b84bbbf8eaba30e6209e13f1082ed2a2222
Alex Lopez <adam23alopez@inslaferreria.cat>
2024-12-09 18:34:34 +0100

    Initial commit

---
+++ src/main/java/com/grupLibros/libro/LibroApplication.java
@@ -0,0 +1,13 @@
+package com.grupLibros.libro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibroApplication.class, args);
+	}
+
+}
