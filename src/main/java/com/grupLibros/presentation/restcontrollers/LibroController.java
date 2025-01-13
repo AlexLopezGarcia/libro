7aee6729fa2fd9ac16ed518a926ac7b85bf6c69f
Alex Lopez <adam23alopez@inslaferreria.cat>
2025-01-13 20:11:03 +0100

    Actulitzat les packages

--- src/main/java/com/grupLibros/presentation/restcontrollers/LibroController.java
+++ src/main/java/com/grupLibros/presentation/restcontrollers/LibroController.java
@@ -1,73 +1,54 @@
 package com.grupLibros.presentation.restcontrollers;
 
 import com.grupLibros.bussiness.model.Libro;
-import com.grupLibros.bussiness.services.LibroServicesImpl;
+import com.grupLibros.bussiness.services.LibroServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/libro")
-
+@RequestMapping("/libros")
 public class LibroController {
+
     @Autowired
-    private LibroServicesImpl libroServices;
+    private LibroServices libroServices;
 
-    // mètode GETall per obtenir tots els usuaris
-    @GetMapping("/libros")
-    public List<Libro> getAll(){// ResponseEntity<Void>
-
-        return null;//llista de productes
+    @GetMapping
+    public List<Libro> getAll() {
+        return libroServices.getAll();
     }
-    @GetMapping("/productos/{id}")
+
+    @GetMapping("/{id}")
     public ResponseEntity<?> read(@PathVariable Long id) {
         Optional<Libro> optional = libroServices.read(id);
-
-        if (optional.isPresent()==false) {
-            RespuestaError respuestaError = new RespuestaError("No se encuentra el producto con id " + id);
-            return new ResponseEntity<>(respuestaError, HttpStatus.NOT_FOUND);
+        if (!optional.isPresent()) {
+            return new ResponseEntity<>("No se encuentra el libro con ID " + id, HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok(optional.get());
     }
 
-    // mètode GET per obtenir un usuari per ID
-    @GetMapping("/{id}")
-    public ResponseEntity<Libro> obtenirUsuariPerId(@PathVariable Long id) {
-        Optional<Libro> libro = libroServices.read(id);
-        return libro.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Libro libro, UriComponentsBuilder ucb) {
+        Long id = libroServices.create(libro);
+        URI uri = ucb.path("/libros/{id}").build(id);
+        return ResponseEntity.created(uri).build();
     }
 
-        // mètode POST per crear un nou usuari
-        @PostMapping
-        public ResponseEntity<Libro> crearUsuari(@RequestBody Libro libro) {
-            Long l = libroServices.create(libro);
-            if(!(l == -1)){
-                return new ResponseEntity<>(libro, HttpStatus.CREATED);
-            } else {
-                return new ResponseEntity<>(libro, HttpStatus.NOT_ACCEPTABLE);
-            }
-        }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        libroServices.delete(id);
+    }
 
-        // mètode PUT per actualitzar un usuari existent
-        @PutMapping("/{id}")
-        public ResponseEntity<Libro> actualitzarUsuari(@PathVariable Long id, @RequestBody Libro libro) {
-            Optional<Libro> usuariActualitzat = libroServices.update(libro);
-            return usuariActualitzat.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-        }
-
-        // mètode DELETE per eliminar un usuari per ID
-        @DeleteMapping("/{id}")
-        public ResponseEntity<Void> eliminarUsuari(@PathVariable Long id) {
-            if (libroServices.delete(id)) {
-                return ResponseEntity.noContent().build();
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        }
-}
+    @PutMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody Libro libro) {
+        libroServices.update(libro);
+    }
 }
